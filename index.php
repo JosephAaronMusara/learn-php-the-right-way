4408ec6afe97b08f337021a91edca66c3d74b84d
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-10 16:58:39 +0200

    operators, 1 new to be specific

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
 <?php
 
+$array = ['a'=>1, 'b'=>null];
+
+var_dump(array_key_exists("", $array)); //Tells you if the key ctually exists in the array
+echo '<pre>';
+
+var_dump(isset($array['b'])); //will tell you if the key exists and is not now
+echo '</pre>';
+
 //Strings
 
 //Heredoc - as if in "" - can have variables
 
-$x=1;
-$y= 2;
+// $x=1;
+// $y= 2;
 
-$text = <<<TEXT
+// $text = <<<TEXT
 
-Line $x
-Line $y
-Line 3
-TEXT;
+// Line $x
+// Line $y
+// Line 3
+// TEXT;
 
-echo nl2br($text);
+// echo nl2br($text);
 
 //Nowdoc - opposite - cant use variables in here, just like we cant with single quotes in normal strings
 //The difference in syntax here is that here the idetifire must be wrapped in single quotes
@@ -29,3 +37,9 @@ echo nl2br($text);
 // //unset($text); - this destroys the variable
 
 // echo nl2br($text);
+
+//ARAYS
+$y =77;
+
+$x =$y ?? 'HELLO'; //x = hello only if y is null, else x = y
+var_dump($x);
