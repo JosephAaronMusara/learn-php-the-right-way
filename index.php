1e270a6feca08a65404c1720180301f5ee3442d9
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-10 15:31:45 +0200

    Heredoc and nowdoc strings

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+//Strings
+
+//Heredoc - as if in "" - can have variables
+
+$x=1;
+$y= 2;
+
+$text = <<<TEXT
+
+Line $x
+line $y
+line 3
+TEXT;
+
+echo nl2br($text);
+
+//Nowdoc - opposite - cant use variables in here, just like we cant with single quotes in normal strings
+//The difference in syntax here is that here the idetifire must be wrapped in single quotes
+
+$text = <<<'TEXT'
+
+Line $x
+line $y
+line 3
+TEXT;
+
+echo nl2br($text);
