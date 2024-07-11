59fbb37d041f77af74b60ae41fddf933f5cd855b
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-11 15:10:45 +0200

    LOOPS

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php $score = 775; ?>
-    <?php if($score>=90 && $score<= 100): ?>
-        <strong style="color: green;">A</strong>
-    <?php elseif($score>=80 && $score< 90): ?>
-        <strong style="color: aqua;">B</strong>
-    <?php elseif($score>=70 && $score< 80): ?>
-        <strong style="color: blue;">C</strong>
-    <?php elseif($score>=0&& $score< 70): ?>
-        <strong style="color: red;">F</strong>
-    <?php else: ?>
-        <strong style="color: darkred; background-color: cyan; font-weight: 900; ">Out of range. Score must be from zero to 100 </strong>
-    <?php endif?>
-</body>
-</html>
+<?php
+//LOOPS
+
+//While
+// $num =0;
+// while($num <=20){
+//     echo $num++ .'<br/>';
+// }
+
+//do-----while
+// $num =25;
+// do{
+//     echo $num-- .'<br/>';
+// }while($num>10);
+
+//for
+// for( $i = 0; $i < 10; $i++ ){
+//     echo "$i <br>";
+// }
+//foreach--works on arrays and objects 'only'. The key variable is not destroyed after
+$programmingLanguages = ['Python', 'PHP','C#','Java','Kotlin'];
+// echo''. implode(', ', $programmingLanguages) .'';
+foreach ($programmingLanguages as $language) {
+    echo "$language<br/>";
+}
+unset($language);
+$language ='Hasvatu';
+echo "<br>$language";
+//So you have to manually unset it 
