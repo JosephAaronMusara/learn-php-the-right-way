11a8c229d1a49c23586bea40f1e4eb0888e6a7a9
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-11 14:38:38 +0200

    A cleaner way of using php if else statement inside of html

--- index.php
+++ index.php
@@ -6,15 +6,17 @@
     <title>Document</title>
 </head>
 <body>
-    <?php $score = 65; ?>
-    <?php if($score>=90):?>
+    <?php $score = 775; ?>
+    <?php if($score>=90 && $score<= 100): ?>
         <strong style="color: green;">A</strong>
-    <?php elseif($score>=80):?>
+    <?php elseif($score>=80 && $score< 90): ?>
         <strong style="color: aqua;">B</strong>
-    <?php elseif($score>=70):?>
+    <?php elseif($score>=70 && $score< 80): ?>
         <strong style="color: blue;">C</strong>
-    <?php else:?>
+    <?php elseif($score>=0&& $score< 70): ?>
         <strong style="color: red;">F</strong>
+    <?php else: ?>
+        <strong style="color: darkred; background-color: cyan; font-weight: 900; ">Out of range. Score must be from zero to 100 </strong>
     <?php endif?>
 </body>
 </html>
