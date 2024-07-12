6c035b4281f17e4b4814c73f428d1d1a28e8996a
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-12 08:20:19 +0200

    require,include and their once

--- uncle.php
+++ uncle.php
@@ -1,7 +1,4 @@
 <?php
-//strict_types only works in the file it is specified so we have to specify
-//it in another file if we want to use the function from another file with it
-declare(strict_types= 1);
-require 'index.php';
+/**require / require_once / include / include_once */
 
-echo sum(7,9);
+//understood lol
