8b8683fdb6fa631ba3ff967ca595b7f4449bdee2
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-16 12:38:02 +0200

    Time Zones And dates

--- uncle.php
+++ uncle.php
@@ -2,7 +2,18 @@
 /* Date & time */
 date_default_timezone_set("Asia/Hong_Kong");
 
-echo date_default_timezone_get() .'<br />';
+echo date_default_timezone_get() .'<br /><br />';
+
+echo date('n/d/Y g:ia',strtotime('tomorrow')) .'<br /><br />';
+echo date('n/d/Y g:ia',strtotime('first day of february')) .'<br /><br />';
+echo date('n/d/Y g:ia',strtotime('last day of march')) .'<br /><br />';
+echo date('n/d/Y g:ia',strtotime('last day of december 2019')) .'<br /><br />';
+$date = date('n/d/Y g:ia',strtotime('second friday of August')) .'<br /><br />';
+
+echo '<pre>';
+print_r(date_parse_from_format('n/d/Y g:ia',$date)) .'<br /><br />';
+echo '</pre>';
+
 
 $currentTime = time();
 
@@ -10,7 +21,7 @@ echo "$currentTime <br />";//current time
 
 echo $currentTime + 5 * 24 * 60 * 60 .'<br />';//5 days from now
 
-echo $currentTime - 24 * 60 * 60 .'<br />'; //yesterday
+echo $currentTime - 24 * 60 * 60 .'<br /><br />'; //yesterday
 
 echo date('n/d/Y g:ia', $currentTime) .'<br />';
 echo date('n/d/Y g:ia', $currentTime + 5 * 24 * 60 * 60) .'<br />';
