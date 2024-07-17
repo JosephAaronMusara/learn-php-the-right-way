c4e655fc4e1aee9091f549af8166efb8f05a2ebb
Joseph Aaron Musara <josephaaronmusara@gmail.com>
2024-07-17 16:10:32 +0200

    file handling

--- fileHandling.php
+++ fileHandling.php
@@ -5,17 +5,23 @@
 
 //checking file existence
 
-if(! file_exists('boom1000.txt')){
+// if(! file_exists('boom1000.txt')){
 
-    echo 'file not found';
+//     echo 'file not found';
     
-}else{
-    $file = fopen('boom1000.txt','r');
-    while(($line = fgets($file)) !== false) {
-        echo "$line <br />";
-    }
-    fclose($file);
+// }else{
+//     $file = fopen('boom1000.txt','r');
+//     while(($line = fgets($file)) !== false) {
+//         echo "$line <br />";
+//     }
+//     fclose($file);
 
-}
-$content = file_get_contents('boom1000.txt');
-echo $content;
+// }
+// $content = file_get_contents('boom1000.txt');
+// echo $content;
+
+// file_put_contents('uncle.txt','pakaipa'); //creating and writing to a file at once
+// file_put_contents('uncle.txt',' zvekutodaro',FILE_APPEND);
+
+// unlink('uncle.txt');//deleting a file
+copy('uncle.txt','joe.docx');// to move or rename, use remane on both files and dirs
